23fdde0a1bf742abcc4e286b130e1130cf9ceec7
kavinkumar06 <kavinm09@gmail.com>
2017-07-12 12:00:06 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+# HelloWorld
+import java.io.*;
+import java.util.*;
+public class 
+{
+  public static void main(String args[])
+  {
+    int n=5;
+    Scanner sc=new Scanner(System.in);
+    n=sc.nextInt();
+    for(int i=0;i<=n;i++)
+    {
+      System.out.println("Hello World"+n);
+      }
+     } 
